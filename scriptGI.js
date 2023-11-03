0d5683e69d28c5035b02b9e001f0d5a7ab18b759
Nyaigoti <osebenyaigoti@gmail.com>
2023-11-03 20:39:48 +0000

    "Styled Pages"

--- scriptGI.js
+++ scriptGI.js
@@ -1,5 +1,5 @@
 // -----------contact form--------
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzjcjds6JdAoTg_v8YlARNpIuL2hKGLb_ctHpZRSILvPsTwM5HEgwb1svZOid97qxtZiA/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxI6SpCGT-duRsI-0F3DLJJmnNOXfZtadUlh80WUM6Q1bf61OzW87S2MaFbPc7DOt2edg/exec'
   const form = document.forms['submit-to-google-sheet']
   const msg = document.getElementById("msg")
 
